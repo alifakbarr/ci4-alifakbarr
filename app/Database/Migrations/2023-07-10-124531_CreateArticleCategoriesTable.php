a796d9da87a8d89dafe623943d9d597f8ce343c4
alifakbarr <alifakbar8800@gmail.com>
2023-07-10 19:56:08 +0700

    add: table article_categories

---
+++ app/Database/Migrations/2023-07-10-124531_CreateArticleCategoriesTable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateArticleCategoriesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'article_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'category_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
+            'updated_at DATETIME DEFAULT CURRENT_TIMESTAMP on update current_timestamp',
+
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('article_id', 'articles', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('category_id', 'categories', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('article_categories');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('article_categories');
+    }
+}
